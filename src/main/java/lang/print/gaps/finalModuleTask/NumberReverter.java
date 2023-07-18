920f0914f996a1d577d61b6acc9591b727ead4f6
martam00 <marta.michniacka1@gmail.com>
2023-07-18 19:09:49 +0200

    final task

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,14 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+
+        int reversed = (number % 10) * 100;
+        number /= 10;
+        reversed += (number % 10) * 10;
+        number /= 10;
+        reversed += number;
+
+        System.out.println(reversed);
+
     }
 }
